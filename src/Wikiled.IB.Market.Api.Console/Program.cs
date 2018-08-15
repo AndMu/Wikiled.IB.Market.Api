8ac77807b9bd73ceb115589ea12f573788c9f319
Andrius Mudinas <andrius@wikiled.com>
2018-08-15 13:57:32 +0100

    Adding console

---
+++ src/Wikiled.IB.Market.Api.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace Wikiled.IB.Market.Api.Console
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await Task.Delay(5000).ConfigureAwait(false);
+        }
+    }
+}
